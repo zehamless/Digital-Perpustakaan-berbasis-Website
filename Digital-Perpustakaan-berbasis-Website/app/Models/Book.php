6fcfbc5fcf22c4450c65538382d8ece1db5c1920
petersonhz25 <82883040+zehamless@users.noreply.github.com>
2023-07-20 17:58:16 +0700

    Update relationship

--- Digital-Perpustakaan-berbasis-Website/app/Models/Book.php
+++ Digital-Perpustakaan-berbasis-Website/app/Models/Book.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
     use HasFactory;
+    protected $fillable=[
+        'title',
+        'description',
+        'amount',
+        'cover',
+        'file_path',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
